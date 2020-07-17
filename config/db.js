d57b185ef263803539d07a9043f4dd5cf5803ff8
mayura-gslab <48903130+mayura-gslab@users.noreply.github.com>
2020-07-17 12:36:13 +0530

    node app

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async() => {
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+    console.log('MongoDB connected!')
+  } catch(e) {
+    console.log(e.message);
+    process.exit();
+  }
+}
+
+module.exports = connectDB;
